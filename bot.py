b90939767b50b9a251860f9c8055c129052c2c8d
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2024-11-17 09:27:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,19 @@
 import logging
-from pyrogram import Client, __version__
+from pyrogram import Client, version
 from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import API_ID, API_HASH, BOT_TOKEN
 from utils import temp
-from typing import Union, Optional, AsyncGenerator
+from typing import Union, Optional
 from pyrogram import types
-from aiohttp import web
+from fastapi import FastAPI
+import asyncio
+import sys
+import uvicorn
+
+app = FastAPI()
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
             name="autofilter",
@@ -28,28 +32,23 @@ class Bot(Client):
         await Media.ensure_indexes()
         me = await self.get_me()
         temp.BOT = self
-        temp.ME = me.id
+        temp.ME = me.id  # Assuming you want the bot ID here instead of a link
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         self.username = '@' + me.username
-        print(f"{temp.U_NAME} start ✅")
-        logging.info(f"Bot started")
-        #web-server
-        # app = web.AppRunner(await web_server())
-        # await app.setup()
-        # port = "5054"
-        # await web.TCPSite(app, "0.0.0.0", port).start()
+        print(f"{temp.U_NAME} start ")
+        logging.info("Bot started.")
 
     async def stop(self, *args):
         await super().stop()
         logging.info("Bot stopped.")
-    
+
     async def iter_messages(
         self,
         chat_id: Union[int, str],
         limit: int,
         offset: int = 0,
-    ) -> Optional[AsyncGenerator["types.Message", None]]:
+    ) -> Optional[asyncio.AsyncGenerator["types.Message", None]]:
         current = offset
         while True:
             new_diff = min(200, limit - current)
@@ -58,8 +57,19 @@ class Bot(Client):
             messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
             for message in messages:
                 yield message
-                current += 1
+            current += 1
 
+bot = Bot()
 
-app = Bot()
-app.run()
+async def main():
+    await bot.start()
+
+@app.get("/healthcheck")
+async def healthcheck():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "bot":
+        asyncio.run(main())
+    elif len(sys.argv) > 1 and sys.argv[1] == "web":
+        uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
